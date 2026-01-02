864dfdf06c83c2db3059be5ce1ea57e086932882
duridev <duridev@outlook.com>
2026-01-02 19:35:07 -0300

    Add initial implementation of Figura class with methods for displaying info and calculating area

---
+++ figuras.py
@@ -0,0 +1,12 @@
+class Figura():
+    def __init__(self,color, nombre):
+        self.color = color
+        self.nombre = nombre
+        
+    def mostrar_info(self, color, nombre):
+        print(f"El nombre es {self.nombre} y el color es {self.color}")
+    
+    def calcular_area():
+        return 0
+    
+    
